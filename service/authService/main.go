10bc5c3d2e2818c7b62cb07e1f52a056576fc9e2
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-03-14 22:24:39 +0530

    auth setup

--- service/authService/main.go
+++ service/authService/main.go
@@ -2,12 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/adityjoshi/E-Commerce-/service/authService/db"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Print("auth service")
 
+	fmt.Print("auth service")
 	db.InitDB()
+
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server := &http.Server{
+		Addr:    ":8001",
+		Handler: router,
+	}
+
+	log.Println("Server is running at :8001...")
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
