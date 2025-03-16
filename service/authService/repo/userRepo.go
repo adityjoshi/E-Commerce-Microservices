4145e8baae5aa795fca4c7aa7b563fcf36f940e3
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-03-16 14:29:38 +0530

    model

--- service/authService/repo/userRepo.go
+++ service/authService/repo/userRepo.go
@@ -1,17 +1,35 @@
 package repo
 
 import (
+	"fmt"
+
 	"github.com/adityjoshi/E-Commerce-/service/authService/db"
 	"github.com/adityjoshi/E-Commerce-/service/authService/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserRepo struct{}
 
 func (u *UserRepo) CreateUser(user *models.Users) error {
 	database := db.GetDB(user.Region)
-	result := database.Create(user)
-	if result.Error != nil {
-		return result.Error
+	if database == nil {
+		return fmt.Errorf("invalid region")
 	}
-	return nil
+	return database.Create(user).Error
+}
+
+func (u *UserRepo) UserLogin(email, password, region string) (*models.UserLogin, error) {
+	database := db.GetDB(region)
+	if database == nil {
+		return nil, fmt.Errorf("invalid region")
+	}
+	var user models.UserLogin
+	if err := database.Where("email=?", email).First(&user).Error; err != nil {
+		return nil, fmt.Errorf("email not found")
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		return nil, fmt.Errorf("invalid password")
+	}
+	return &user, nil
 }
