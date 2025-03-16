c9f5fc122210614cdd7c099098c07fb6c1001076
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-03-16 14:41:06 +0530

    model

--- service/authService/utils/jwt.go
+++ service/authService/utils/jwt.go
@@ -2,14 +2,14 @@ package utils
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/adityjoshi/E-Commerce-/service/authService/models"
-	"github.com/golang-jwt/jwt"
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var jwtKey = []byte("JWT123")
+var jwtKey = []byte(os.Getenv("JWTSECRET"))
 
 func GetJWTKey(user *models.Users) (string, error) {
 	claims := jwt.MapClaims{
@@ -25,4 +25,18 @@ func GetJWTKey(user *models.Users) (string, error) {
 		return "", fmt.Errorf("could not sign token: %v", err)
 	}
 	return tokenString, nil
+}
+
+func DecodeJwt(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, fmt.Errorf("invalid token")
 }
